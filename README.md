2d0afd8e302153e7313cb846ba2ed6742e10f3f2
Nevena Alavanja <nevena.alavanja@levi9.com>
2023-04-05 00:24:39 +0200

    Corrected grammar and added poll and quiz examples

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# QuesTech
-DSL for making different types of assasaments.
+<h1>QuesTech</h1>
+<br/>
+<span>DSL for making different types of assessments.</span>
+<br/>
+<h3>Usage</h3>
+[Optional] Create virtual environment,
+e.g.
+<ol>
+    <li>python -m venv venv</li>
+    <li>
+        <ul>
+            <li><b>Windows:</b> .\venv\Scripts\activate</li>
+            <li><b>Linux:</b>&emsp;&emsp;source .\venv\bin\activate</li>
+        </ul>
+    </li>
+</ol>
+Installation
+<ol>
+  <li>
+     <b>Install textX:</b> pip install textx[cli]
+  </li>
+  <li>
+    <b>Create visualization (GraphViz is needed): </b>
+        textx generate grammar.tx --target dot
+        dot -Tpng -O grammar.dot
+  </li>
+</ol>
+<br/>
+Running
+<ol>
+    <li>Run application with "python main.py" to use the default assessment file = quiz.qt</li>
+    <li>To run other assessment, run with "python main.py assessment_file=[path-to-some-other-assessment-file]"</li>
+</ol>
---
+++ main.py
@@ -0,0 +1,45 @@
+from os.path import join, dirname, abspath
+from textx import metamodel_from_file
+from textx.export import metamodel_export, model_export
+import os
+import pydot
+import click
+
+
+class Assessment():
+    def __init__(self, assessment, assessment_mm, assessment_file):
+        self.assessment = assessment
+        self.assessment_mm = assessment_mm
+        self.assessment_file = assessment_file
+
+
+@click.command()
+@click.option('--assessment_file', default="quiz.qt", help="Full or relative path to the assessment file")
+def main(assessment_file):
+    this_folder = dirname(__file__)
+    assessment_mm = metamodel_from_file(join(this_folder, 'grammar.tx'))
+    dot_folder = join(this_folder, 'dot_files')
+
+    if not os.path.exists(dot_folder):
+        os.mkdir(dot_folder)
+    metamodel_export(assessment_mm, join(dot_folder, 'grammar.dot'))
+    (graph,) = pydot.graph_from_dot_file(join(dot_folder, 'grammar.dot'))
+    graph.write_png(join(dot_folder, 'grammar.png'))
+
+    # try:
+    assessment: Assessment = assessment_mm.model_from_file(
+        join(abspath(this_folder), assessment_file))
+    print(assessment.assessment_details.questions[0].question_details.type)
+    print(
+        assessment.assessment_details.questions[0].question_details.answers[1].text)
+    # except:
+    #     try:
+    #         assessment: Assessment = assessment_mm.model_from_file(
+    #             assessment_file)
+    #     except:
+    #         print("The assessment file path is not valid")
+    #         return
+
+
+if __name__ == "__main__":
+    main()
